b28e2523c1fa0f564a660b901dabb0770b74b0e4
im725896 <im725896@iteso.mx>
2025-10-23 19:19:26 -0600

    Ajuste de pdf de la práctica y subida de códigos finales de cada capitulo

---
+++ PRACTICA-01/Capitulo-3/Funciones_Codigos.py
@@ -0,0 +1,141 @@
+import cv2
+import cv2.aruco as aruco
+import pandas as pd
+import numpy as np
+import json
+import os
+from datetime import date
+
+ARCHIVO_PARAMETROS_CAMARA = "parametros_camara.json"
+ARCHIVO_VALORES_CSV ="valores.csv"
+CARPETA_FOTOS_CALIBRACION = "Fotos_Calibracion"
+CARPETA_CODIGOS_ARUCO = "Codigos_ArUco"
+RESOLUCION_CAMARA = {
+    "RESOLUCION_WIDTH": 1280,
+    "RESOLUCION_HEIGHT": 720
+}
+
+def filtrar_Archivos_JPG():
+    dir_carpeta_imagenes = juntar_DireccionAbs_Archivo(CARPETA_FOTOS_CALIBRACION)
+    imagenes = [os.path.join(dir_carpeta_imagenes, f)
+                for f in os.listdir(dir_carpeta_imagenes)
+                if f.lower().endswith('.jpg')]
+    
+    return imagenes
+
+def guardar_Resolucion_Camara(width, height):
+    RESOLUCION_CAMARA["RESOLUCION_WIDTH"] = width
+    RESOLUCION_CAMARA["RESOLUCION_HEIGHT"] = height
+
+def obtener_Nombre_Carpeta_Fotos():
+    return CARPETA_FOTOS_CALIBRACION
+
+def crear_Carpeta_Fotos():
+    crear_Carpeta(CARPETA_FOTOS_CALIBRACION)
+
+def crear_Carpeta_ArUco():
+    crear_Carpeta(CARPETA_CODIGOS_ARUCO)
+
+def crear_Carpeta(nombre_carpeta):
+    direccion_carpeta = juntar_DireccionAbs_Archivo(nombre_carpeta)
+    os.makedirs(direccion_carpeta, exist_ok=True)
+    
+    if os.path.exists(direccion_carpeta):
+        print(f"Se creo la carpeta '{nombre_carpeta}'")
+    else:
+        print(f"Error al crear la carpeta '{nombre_carpeta}'")
+
+def guardar_Foto_Calibracion(imagen, contador):
+    nombre_foto = f"foto_ajedrez_{contador:02}.jpg"
+    
+    guardar_Imagen(CARPETA_FOTOS_CALIBRACION, nombre_foto, imagen)
+    
+    print(f"Foto guardada como {nombre_foto}")
+
+def guardar_Codigo_ArUco(imagen_codigo, id):
+    nombre_aruco = f"Codigo_Aruco_{id:02}.jpg"
+    
+    guardar_Imagen(CARPETA_CODIGOS_ARUCO, nombre_aruco, imagen_codigo)
+
+    print(f"Se creo el código ArUco con id: {id}")
+
+def guardar_Imagen(direccion_carpeta, nombre_archivo, imagen):
+    direccion_nombre = juntar_Direccion1_Direccion2(
+        juntar_DireccionAbs_Archivo(direccion_carpeta), nombre_archivo 
+        )
+    
+    cv2.imwrite(direccion_nombre, imagen)
+
+def guardar_Parametros_Camara(matriz_camara, coeficientes_distorsion):
+    
+    estructura_datos = {
+        "fecha_calibracion" : str(date.today()),
+        "resolucion_width" : int(RESOLUCION_CAMARA["RESOLUCION_WIDTH"]),
+        "resolucion_height" : int(RESOLUCION_CAMARA["RESOLUCION_HEIGHT"]),
+        "matriz_camara" : matriz_camara.tolist(),
+        "coef_dist" : coeficientes_distorsion.tolist()
+    }
+
+    direccion = juntar_DireccionAbs_Archivo(ARCHIVO_PARAMETROS_CAMARA)
+
+    if os.path.exists(direccion):
+        datos = obtener_Parametros_Camara()
+        datos.update(estructura_datos)
+    else:
+        datos = estructura_datos
+
+    with open(direccion, "w") as f:
+        json.dump(datos, f, indent=4) 
+
+def guardar_Parametros_Extrinsecos_Camara(matriz_rotacion, vector_traslancion):
+    estructura_datos = {
+        "matriz_rotacion" : matriz_rotacion.tolist(),
+        "vector_traslacion" : vector_traslancion.tolist()
+    }
+
+    parametros_camara = obtener_Parametros_Camara()
+    parametros_camara.update(estructura_datos)
+
+    direccion = juntar_DireccionAbs_Archivo(ARCHIVO_PARAMETROS_CAMARA)
+    
+    with open(direccion, "w") as f:
+        json.dump(parametros_camara, f, indent = 4)
+
+def obtener_Parametros_Camara():
+    direccion = juntar_DireccionAbs_Archivo(ARCHIVO_PARAMETROS_CAMARA)
+
+    if not os.path.exists(direccion):
+        raise FileNotFoundError(f"No se encontró el archivo de parámetros: {direccion}")
+
+    with open(direccion, "r") as f:
+        parametros_camara = json.load(f)
+
+    return parametros_camara
+
+def guardar_Datos_CSV(datos):
+    if not datos:
+        raise ValueError(f"Los datos proporcionados para guardar en CSV están vacíos.")
+
+    direccion = juntar_DireccionAbs_Archivo(ARCHIVO_VALORES_CSV)
+    df = pd.DataFrame(datos)
+    df.to_csv(direccion, index = False)
+
+def sacar_Datos_CSV():
+    direccion = juntar_DireccionAbs_Archivo(ARCHIVO_VALORES_CSV)
+
+    if not os.path.exists(direccion):
+        raise FileNotFoundError(f"No se encontró el archivo CSV: {direccion}")
+    
+    return pd.read_csv(direccion)
+
+def obtener_Direccion_Carpeta_Absoluta():
+    try:
+        return os.path.dirname(os.path.abspath(__file__))
+    except NameError:
+        return os.getcwd()
+
+def juntar_DireccionAbs_Archivo(nombre_archivo):
+    return os.path.join(obtener_Direccion_Carpeta_Absoluta(), nombre_archivo)
+
+def juntar_Direccion1_Direccion2(direccion_carpeta, nombre_archivo):
+    return os.path.join(direccion_carpeta, nombre_archivo)
---
+++ PRACTICA-01/Capitulo-5/Calibracion_Extrinseca_Camara.py
@@ -0,0 +1,100 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import Funciones_Codigos as fc
+
+# ------ Parámetros generales ------
+tamaño_aruco = 0.143  # en metros
+coordenadas_world = {
+    1: np.array([0.0, 0.0, 0.0]),   # Esquina inferior izq.
+    2: np.array([2.554, 0.0, 0.0]), # Esquina inferior der.
+    3: np.array([0.0, 1.803, 0]),   # Esquina superior izq.
+    5: np.array([2.554, 1.803, 0])  # Esquina superior der.
+}
+
+# ------ Configuración códigos ArUco ------
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters()
+detector = aruco.ArucoDetector(aruco_dict, parameters)
+
+# ------ Obtención de los parámetros de la cámara ------
+parametros_camara = fc.obtener_Parametros_Camara()
+
+def obtener_imagen_camara(camara):
+    ret, imagen = camara.read()
+    if not ret:
+        print("No se pudo tomar una imagen.")
+        exit()
+    return imagen
+
+def detectar_codigos(imagen):
+    obj_points = []  # Puntos 3D conocidos
+    img_points = []  # Puntos 2D detectados en la imagen
+
+    corners, ids, _ = detector.detectMarkers(imagen)
+    if ids is None:
+        print("No se detectaron ArUcos en la imagen.")
+        exit()
+
+    for i, marker_id in enumerate(ids.flatten()):
+        if marker_id in coordenadas_world:
+            c = corners[i][0]
+            centro = c.mean(axis=0)
+            img_points.append(centro)
+            obj_points.append(coordenadas_world[marker_id])
+
+    obj_points = np.array(obj_points, dtype=np.float32)
+    img_points = np.array(img_points, dtype=np.float32)
+
+    return obj_points, img_points
+
+def obtener_rotacion_traslacion(obj_points, img_points, parametros_camara):
+    _, rvec, tvec = cv2.solvePnP(
+        obj_points, img_points,
+        np.array(parametros_camara["matriz_camara"]),
+        np.array(parametros_camara["coef_dist"])
+    )
+
+    R, _ = cv2.Rodrigues(rvec)
+    print("Matriz de rotación (R):\n", R)
+    print("Vector de traslación (t):\n", tvec)
+
+    fc.guardar_Parametros_Extrinsecos_Camara(R, tvec)
+    print("Se guardaron la R y t en el archivo json")
+
+    return rvec, tvec
+
+def dibujar_ejes(imagen, rvec, tvec, img_points, parametros_camara):
+    axis = np.float32([[0.1,0,0], [0,0.1,0], [0,0,-0.1]])
+    imgpts, _ = cv2.projectPoints(
+        axis, rvec, tvec,
+        np.array(parametros_camara["matriz_camara"]),
+        np.array(parametros_camara["coef_dist"])
+    )
+
+    corner = tuple(img_points[0].astype(int))  # primer punto detectado
+    imagen = cv2.line(imagen, corner, tuple(imgpts[0].ravel().astype(int)), (0,0,255), 3) # X
+    imagen = cv2.line(imagen, corner, tuple(imgpts[1].ravel().astype(int)), (0,255,0), 3) # Y
+    imagen = cv2.line(imagen, corner, tuple(imgpts[2].ravel().astype(int)), (255,0,0), 3) # Z
+
+    cv2.imshow("Extrinseca con 4 ArUco", imagen)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# --- Captura de imagen y procesamiento ---
+camara = cv2.VideoCapture(1)
+camara.set(cv2.CAP_PROP_FRAME_HEIGHT, parametros_camara["resolucion_height"])
+camara.set(cv2.CAP_PROP_FRAME_WIDTH, parametros_camara["resolucion_width"])
+
+imagen = obtener_imagen_camara(camara)
+
+obj_points, img_points = detectar_codigos(imagen)
+
+# --- Obtenemos la rotación y traslación ---
+rvec, tvec = obtener_rotacion_traslacion(obj_points, img_points, parametros_camara)
+
+# --- Dibujar ejes en un ArUco de referencia ---
+dibujar_ejes(imagen, rvec, tvec, img_points, parametros_camara)
+
+camara.release()
+cv2.destroyAllWindows()
